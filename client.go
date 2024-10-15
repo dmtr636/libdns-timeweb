2ff22746d7eddd192262096f76045ba2bacefd96
dmtr636 <dmtr636@gmail.com>
2024-10-15 17:12:43 +0300

    Test

--- client.go
+++ client.go
@@ -56,7 +56,7 @@ func (p *Provider) doAPIRequest(req *http.Request, result interface{}) error {
 	body, err := io.ReadAll(response.Body)
 
 	if response.StatusCode >= 400 {
-		return fmt.Errorf("got error status: HTTP %d: %+v", response.StatusCode, string(body))
+		return fmt.Errorf("got error status: HTTP %d: url: %s: %+v", response.StatusCode, req.URL, string(body))
 	}
 
 	if response.StatusCode == http.StatusNoContent {
